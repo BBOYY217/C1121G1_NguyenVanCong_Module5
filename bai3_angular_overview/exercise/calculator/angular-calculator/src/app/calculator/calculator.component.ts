ad6bcf14b9e6f18127a29492b7f4eaaf9155b147
Công Nguyễn <nguyenvancong2107@gmail.com>
2022-05-09 16:18:16 +0700

    up

---
+++ bai3_angular_overview/exercise/calculator/angular-calculator/src/app/calculator/calculator.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+    result: number;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  calculator(a: number,b: number,cal: string ){
+    switch (cal) {
+      case '+':
+        return this.result = a + b ;
+      case '-':
+        return this.result = a - b ;
+      case '*':
+        return this.result = a * b ;
+      case '/':
+        return this.result = a / b ;
+    }
+  }
+}
--- case-study/angular-case-study/src/app/app-routing.module.ts
+++ case-study/angular-case-study/src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import {BodyComponent} from './body/body.component';
 import {ListEmployeeComponent} from './employee/list-employee/list-employee.component';
 import {ListContractComponent} from './contract/list-contract/list-contract.component';
 import {ListServiceComponent} from './service/list-service/list-service.component';
+import {ListFacilitiesComponent} from './facility/list-facilities/list-facilities.component';
+import {CreateCustomerComponent} from './customer/create-customer/create-customer.component';
 
 
 const routes: Routes = [
   {path:"list-customer",component:ListCustomerComponent},
+  {path:"create-customer",component:CreateCustomerComponent},
   {path:"list-employee",component:ListEmployeeComponent},
   {path:"list-service",component:ListServiceComponent},
   {path:"list-contract",component:ListContractComponent},
+  {path:"list-facilities",component:ListFacilitiesComponent},
   {path:"", component: BodyComponent}
 ];
 
