e1c5eb06d9287ecc093b5a89a105333181432cc1
Công Nguyễn <nguyenvancong2107@gmail.com>
2022-05-15 19:56:36 +0700

    up

--- bai6_angular_form/exercise/login/angular-login/src/app/login/login.component.ts
+++ bai6_angular_form/exercise/login/angular-login/src/app/login/login.component.ts
@@ -7,13 +7,11 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  login: FormGroup;
-
-  constructor() {
-  }
+  loginForm: FormGroup;
+  constructor() { }
 
   ngOnInit(): void {
-    this.login = new FormGroup({
+    this.loginForm = new FormGroup({
       username: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
